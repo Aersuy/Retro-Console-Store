ab0ddb5e365e099855ba826625f7f20cef832598
Aersuy <raileanu1100@gmail.com>
2025-02-25 11:35:14 +0200

    Added bundle

--- RetroConsoleStoreDotWeb/App_Start/BundleConfig.cs
+++ RetroConsoleStoreDotWeb/App_Start/BundleConfig.cs
@@ -10,29 +10,22 @@ using System.Web.Routing;
 
 namespace RetroConsoleStoreDotWeb.App_Start
 {
-    public class Global : HttpApplication
-    {
-        void Aplication_Start(object sender, EventArgs e)
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-
-            BundleConfig.RegisterBundle(BundleTable.Bundles);
-        }
-    }
+    
     public class BundleConfig
     {
         public static void RegisterBundle(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~bundles/bootstrap/css").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
 
-            bundles.Add(new ScriptBundle("~bundles/bootstrap/js").Include("~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include("~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~bundles/font-awesome/css").Include("~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include("~/Scripts/jquery-1.10.2.min.js"));
 
-            bundles.Add(new StyleBundle("~bundles/toaster/css").Include("~/Content/toastr.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include("~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
 
-            bundles.Add(new StyleBundle("~bundles/datatables/css").Include("~/Vendors/datatables/datatables.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/toastr/css").Include("~/Content/toastr.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/datatables/css").Include("~/Vendors/datatables/datatables.min.css", new CssRewriteUrlTransform()));
         }
     }
 }
