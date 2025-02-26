623c280a555248e4bd119b4db775a82fd65d510c
Aersuy <raileanu1100@gmail.com>
2025-02-26 13:28:59 +0200

    gjhgh

--- RetroConsoleStoreDotWeb/App_Start/BundleConfig.cs
+++ RetroConsoleStoreDotWeb/App_Start/BundleConfig.cs
@@ -19,13 +19,21 @@ namespace RetroConsoleStoreDotWeb.App_Start
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include("~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include("~/Scripts/jquery-1.10.2.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include("~/Scripts/jquery-3.2.1.min.js"));
 
             bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include("~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/toastr/css").Include("~/Content/toastr.min.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/datatables/css").Include("~/Vendors/datatables/datatables.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/animate/css").Include("~/content/animate.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/style/css").Include("~/content/style.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts/js").Include("~/Scripts/scripts.js"));
+
+
         }
     }
 }
