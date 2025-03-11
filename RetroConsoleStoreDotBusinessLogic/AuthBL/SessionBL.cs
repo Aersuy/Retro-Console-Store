9b0ea8e2aeae1e8e40f940d0e34074c2dfb73b55
Aersuy <raileanu1100@gmail.com>
2025-03-11 10:45:17 +0200

    Added the 3 other projects

---
+++ RetroConsoleStoreDotBusinessLogic/AuthBL/SessionBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetroConsoleStoreDotBusinessLogic.Core;
+using RetroConsoleStoreDotBusinessLogic.Interfaces;
+
+namespace RetroConsoleStoreDotBusinessLogic.AuthBL
+{
+    internal class SessionBL : UserAPIcs, ISessionBL
+    {
+
+    }
+}
---
+++ RetroConsoleStoreDotBusinessLogic/BL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetroConsoleStoreDotBusinessLogic.Interfaces;
+
+namespace RetroConsoleStoreDotBusinessLogic
+{
+    internal class BL
+    {
+        public ISessionBL SessionBL()
+        {
+            return new AuthBL.SessionBL();
+        }
+    }
+}
