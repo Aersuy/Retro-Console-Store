b12a27d4308cf16541208e9c1da3d1d8ce9348d2
Aersuy <raileanu1100@gmail.com>
2025-03-22 18:40:07 +0200

    Added item controller

---
+++ RetroConsoleStoreDotWeb/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RetroConsoleStoreDotWeb.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Catlog()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
