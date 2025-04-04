b75e05f6721056df9d6119284e716bb231ad24e8
Aersuy <raileanu1100@gmail.com>
2025-04-04 13:59:37 +0300

    .

---
+++ RetroConsoleStoreDotDomain/Logs/LTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetroConsoleStoreDotDomain.User;
+
+namespace RetroConsoleStoreDotDomain.Logs
+{
+    public class LTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LogId { get; set; }
+
+
+        [Required]
+        public int UserId { get; set; }
+
+
+        [Required]
+        public string UserName { get; set; }
+
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public string UserIp {  get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual UDBTablecs User { get; set; }
+
+
+    }
+}
