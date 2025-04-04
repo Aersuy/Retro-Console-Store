93c27e7eb7cf9e866ff0736fae343565bc145f04
Aersuy <raileanu1100@gmail.com>
2025-04-04 12:12:08 +0300

    Added log database

---
+++ RetroConsoleStoreDotBusinessLogic/DBContext/LogContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetroConsoleStoreDotDomain.Logs;
+
+namespace RetroConsoleStoreDotBusinessLogic.DBContext
+{
+    public class LogContext : DbContext
+    {
+        public LogContext() : base("name=Retro-Console-Store-Log") { }
+        public virtual DbSet<LTable> Logs { get; set; }
+    }
+
+}
