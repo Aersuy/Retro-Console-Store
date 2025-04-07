97f650fbe2a6bb9b7a3e6f915b9945cebb4ac010
Aersuy <raileanu1100@gmail.com>
2025-04-07 15:44:18 +0300

    added password hashing package

---
+++ RetroConsoleStore.BusinessLogicTests/BL struct/AuthBLTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RetroConsoleStore.BusinessLogic.BL_Struct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroConsoleStore.BusinessLogic.BL_Struct.Tests
+{
+    [TestClass()]
+    public class AuthBLTests
+    {
+        [TestMethod()]
+        public void UserAuthLogicTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
