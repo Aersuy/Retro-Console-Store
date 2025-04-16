3469020d8496200bb430127b7c82a7ea1f62f520
Nicu_Iurie <iurie.nicu@isa.utm.md>
2025-04-16 17:43:44 +0300

    Finished product EF table

--- RetroConsoleStoreDotDomain/Products/PhysicalProductT.cs
+++ RetroConsoleStoreDotDomain/Products/PhysicalProductT.cs
@@ -15,7 +15,17 @@ namespace RetroConsoleStoreDotDomain.Products
         public int DatabaseID { get; set; }
 
         [Required]
+        [ForeignKey("ProductTypeT")]
+        public int ProductTypeTId { get; set; }
+        public string SerialNumber { get; set; }   
+        public bool IsUsed { get; set; } // true if used, false if new
+        public string Condition { get; set; } // New, Like New, Good, Acceptable
+        public string BoxCondition { get; set; } // New, Like New, Good, Acceptable
+        public string InformationAboutPrivousOwner { get; set; } // Information about the previous owner, if any
 
-        
+
+
+
+
     }
 }
