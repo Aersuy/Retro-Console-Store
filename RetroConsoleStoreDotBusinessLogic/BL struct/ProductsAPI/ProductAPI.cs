e138aece3fee2aeef8d40b37846125519619d765
Aersuy <raileanu1100@gmail.com>
2025-04-17 13:39:56 +0300

    .

---
+++ RetroConsoleStoreDotBusinessLogic/BL struct/ProductsAPI/ProductAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetroConsoleStoreDotBusinessLogic.Interfaces;
+
+namespace RetroConsoleStoreDotBusinessLogic.BL_struct.ProductsAPI
+{
+    internal class ProductAPI : IProduct
+    {
+
+    }
+}
--- RetroConsoleStoreDotBusinessLogic/Interfaces/IProduct.cs
+++ RetroConsoleStoreDotBusinessLogic/Interfaces/IProduct.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RetroConsoleStoreDotBusinessLogic.BL_struct.CartAPI
+namespace RetroConsoleStoreDotBusinessLogic.Interfaces
 {
-    public class CartAPI
+    internal interface IProduct
     {
+        bool AddProduct()
     }
 }
