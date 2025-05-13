66f1ac2a2904dfacef6535ecb39b12ee8adf179a
Nicu_Iurie <iurie.nicu@isa.utm.md>
2025-05-13 13:20:14 +0300

    .

---
+++ RetroConsoleStoreDotDomain/Model/User/UserCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetroConsoleStoreDotDomain.Model.Product;
+
+namespace RetroConsoleStoreDotDomain.Model.User
+{
+    class UserCart
+    {
+        public List<CartItemModel> CartItems { get; set; }
+    }
+}
